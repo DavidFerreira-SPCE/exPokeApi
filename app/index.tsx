2bb46ebec6b959e96267bc05e1267dafeba3a7e9
David Ferreira <davidleandro670@gmail.com>
2025-11-11 11:00:54 -0300

    V1 muitas mudanças que não cabem em um commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,61 @@
 import { Image } from 'expo-image';
-import React from 'react';
-import { ScrollView, View } from "react-native";
+import React, { useState } from 'react';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Styles from './styles';
+import axios from 'axios'; 
 
-export default function Index() {
-  return (
-    <SafeAreaView style={Styles.body}>
-      <ScrollView>
+export default function SearchPokemon() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [digimonData, setDigimonData] = useState(null);
+  const [initialDigimons, setInitialDigimons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  
+    try {
+    const requests = await axios.get('https://pokeapi.co/api/v2/pokemon/{id or name}/');
+    if (requests.data.content && requests.data.content.length > 0) {
+      setInitialPokes(prevPokes => [...prevPokes, ...requests.data.content]);
+      setCurrentPage(requests.data.pageable.currentPage);
+
+    }
+
+
+return (
+  <SafeAreaView style={Styles.body}>
+    <ScrollView style={Styles.containerBody}>
       <View>
-      <Image
-          source={require("../assets/images/pokeapi.png")}
-          style={Styles.logo}
-      />
-      </View>
-      <View style={}>
-      <Text style={}>Welcome to PokeApi Unofficial Searching App</Text>
+        <View style={Styles.logoDisplay}>
+          <Image
+            source={require("../assets/images/pokeapi.png")}
+            style={Styles.logo}
+          />
+        
+          <Image
+            source={require("../assets/images/pokebola.png")}
+            style={Styles.logo}
+          />
+        </View>
+        <Text style={Styles.texto}>
+          Welcome to PokeApi Unofficial Searching App
+        </Text>
 
-      <TextInput
-          style={}
+        <TextInput
+          style={Styles.textoBusca}
           placeholder='Digite o nome/número do Pokemon que deseja encontrar'
+          placeholderTextColor={'white'}
           value={userInput}
           onChangeText={setUserInput}
           onSubmitEditing={() => searchPoke()}
         />
+        <TouchableOpacity
+        style={Styles.julioBox}
+        >
+          <Text style={Styles.julio}>Pesquisar</Text>
+        </TouchableOpacity>
       </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+    </ScrollView>
+  </SafeAreaView>
+)};
--- app/styles.ts
+++ app/styles.ts
@@ -2,26 +2,67 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     body: {
+        backgroundColor: '#ca1111de',
         flex: 1,
+        flexDirection: 'column',
         justifyContent: "center",
         alignItems: "center",
     },
-    logo: {
-        resizeMode: 'contain',
-        width: 70,
-        height: 60,
-        marginTop: 'auto',
-        marginRight: 'auto'
+    containerBody: {
+        backgroundColor: '#969696ff',
+        height: '90%',
+        width: '80%',
+        borderRadius: 32
     },
-<<<<<<< HEAD
+    logoDisplay: {
+        flexDirection: 'row',
+        justifyContent:'center',
+        gap: 100,
+        alignItems: 'center',
+        width: '100%',
+    },
+    logo: {   
+        resizeMode: 'contain',
+        width: 90,
+        height: 120,
+    },
     texto: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginBottom: 15,
-    color: '#ff0000ff',
-  },
-=======
->>>>>>> 5f13504fc933f425d3a0ee30ee4fb15a7861ddc1
+        padding: 12,
+        margin: 10,
+        display:'flex',
+        backgroundColor: '#757575ff',
+        color: 'yellow',
+        fontSize: 18,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        justifyContent:'center',
+        textAlign:'center',
+        borderRadius:8
+    },
+    textoBusca: {
+        backgroundColor: '#585633b4',
+        color: '#e7e7e7ff',
+        textAlign: 'center',
+        width: 'auto',
+        padding: 20,
+        fontSize: 16,
+        margin: 12,
+        marginBottom: 10,
+        borderRadius: 12
+    },
+    julio: {
+        color:'white',
+        fontSize: 16,
+        display: 'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: '#a01010ff',
+        height: '90%',
+        width: '50%',
+        margin:'auto',
+        padding: 10,
+        borderRadius: 16
+    },
 })
 
 export default styles
