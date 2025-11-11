8699eb8361b5bed58fbe1ca89844b023e8514c31
David Ferreira <davidleandro670@gmail.com>
2025-11-11 07:18:53 -0300

    Iniciando aos poucos

---
+++ app/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    logo: {
+        resizeMode: 'contain',
+        width: 70,
+        height: 60,
+        marginTop: 'auto',
+        marginRight: 'auto'
+    },
+    texto: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 15,
+    color: '#ff0000ff',
+  },
+})
+
+export default styles
